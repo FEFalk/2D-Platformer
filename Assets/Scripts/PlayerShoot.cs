7cde5ced588db8c014b63faa4a7a427e3aba2a68
Frenning <fredrikfrenning@gmail.com>
2015-04-21 20:39:48 +0200

    Bullet

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+    public Vector3 bulletOffset = new Vector3(0.5f, 0, 0);
+
+	void Update () {
+        cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            Debug.Log("PeW!");
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset; 
+            Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+        }
+	}
+}
