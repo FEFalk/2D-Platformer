50f040b4fb14175e8fb3aa9790c3c366d877cde3
FEFalk <fille_945@hotmail.com>
2015-05-14 20:51:07 +0200

    Lazerpekare

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,21 +2,38 @@
 using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
-    public GameObject bulletPrefab;
-    public float fireDelay = 0.25f;
-    float cooldownTimer = 0;
-    public Vector3 bulletOffset = new Vector3(0.5f, 0, 0);
+    public GameObject laserPrefab;
+    private Vector3 startPosition;
+    public int rotationOffset = 0;
+    private float scaling;
 
-	void Update () {
-        cooldownTimer -= Time.deltaTime;
+    void Start()
+    {
+        startPosition = laserPrefab.transform.position;
+    }
 
-        if (Input.GetButton("Fire1") && cooldownTimer <= 0)
+    void Update () {
+        
+
+        if (Input.GetButton("Fire1"))
         {
-            Debug.Log("PeW!");
-            cooldownTimer = fireDelay;
 
-            Vector3 offset = transform.rotation * bulletOffset; 
-            Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize();
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+
+            scaling += 2;
+            laserPrefab.transform.position = new Vector3(transform.position.x + scaling, 0);
+            laserPrefab.transform.localScale = new Vector3(transform.localScale.x + scaling, 1, 0);
+
         }
-	}
+        else if (!Input.GetButton("Fire1"))
+        {
+            scaling = 0;
+            laserPrefab.transform.localScale = new Vector2(0, 1);
+            laserPrefab.transform.position = startPosition;
+        }
+
+    }
 }
