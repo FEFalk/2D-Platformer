4c54bb13a5fcabafa9ef24d5db945a510ba72e50
FEFalk <fille_945@hotmail.com>
2015-05-28 21:04:22 +0200

    Level 2 och 1, design, ljud osv.

--- Assets/Scripts/ObstacleHandler.cs
+++ Assets/Scripts/ObstacleHandler.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class ObstacleHandler : MonoBehaviour {
     public bool activated = false;
+    public GameObject AlarmLights;
     void Update()
     {
         if (activated) 
         {
-            Destroy(gameObject);
-            activated = false;
+            StartCoroutine(Activate());
+
         }
+    }
+    IEnumerator Activate()
+    {
+        gameObject.GetComponent<Animator>().enabled = true;
+        AlarmLights.SetActive(true);
+        AlarmLights.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(2);
+        GetComponent<Collider2D>().enabled = false;
+        
+        GameObject.Find("WindowBackground").gameObject.GetComponent<Animator>().enabled = true;
+        activated = false;
+
+        GameObject.Find("GameManager").gameObject.GetComponent<AudioSource>().Play();
     }
 
 }
