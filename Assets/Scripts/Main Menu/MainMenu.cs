4050622273069f7a82f60a3776a2dbe2958a88da
henbobk <henning.boberg@hotmail.com>
2015-05-30 22:33:51 +0200

    Main Menu

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel(1);
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        if (isQuit)
+            Application.Quit();
+    }
+}
